ef77fdd2b2795b71e00e0c091d8ead79fc9bfe96
chang-soo0P <onlvtst22@gmail.com>
2025-12-11 16:34:58 +0900

    Telma 폰트 설치 + layout 파일 수정-turbopack에서 상대경로 사용하여 깨짐현상 발생하여 절대경로로 변경

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,22 +7,22 @@ import { Toaster } from "@/components/ui/toaster"
 const telma = localFont({
   src: [
     {
-      path: "../public/fonts/Telma-Regular.woff2",
+      path: "/fonts/Telma-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../public/fonts/Telma-Medium.woff2",
+      path: "/fonts/Telma-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../public/fonts/Telma-SemiBold.woff2",
+      path: "/fonts/Telma-SemiBold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../public/fonts/Telma-Bold.woff2",
+      path: "/fonts/Telma-Bold.woff2",
       weight: "700",
       style: "normal",
     },
